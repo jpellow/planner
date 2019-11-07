8337a3b1e6bb47c42237ad1ee339920172e48791
Jeremy Pellow <jeremy.pellow@gmail.com>
2019-11-07 08:17:13 -0600

    added HTML, CSS and JS code. rows generate when the page loads and populates local storge if it exists. NEED TO COMMIT MORE!

--- script.js
+++ script.js
@@ -2,25 +2,32 @@ $(document).ready(function () {
 
     load();
     function load(){
+        localStorage.setItem("e10", "do things");
         for (i=9; i < 18; i++){
         var newRow = $("<div>").addClass("row");
         var hour = $("<div>").addClass("col-1 text-right");
         var task = $("<input>").addClass("col-10 form-control");
         var save = $("<button>").addClass("btn btn-info col-1 text-center");
         hour.attr("id", "hour");
-        task.attr({id: "task", type:"text"});
+        task.attr({id: "task", type:"text", data: "e"+i});
+        task.val(localStorage.getItem("e"+i))
         save.attr({type: "button", id: "save"});
         save.text("Save");
-        if(i > 12){
+        if(i >= 12){
+            if (i === 12){
+                hour.text(i + "PM");
+            }
+            else{
             hour.text((i - 12) + "PM");
+            }
         }
         else{
             hour.text(i + "AM");
         }
         $("#hourRows").append(newRow);
-        $(newRow).append(hour);
-        $(newRow).append(task);
-        $(newRow).append(save);
+        $(newRow).append(hour, task, save);
+        // $(newRow).append(task);
+        // $(newRow).append(save);
         };
         i++
     }
