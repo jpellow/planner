0e912dd62ed1f23d7ad1ff19c3476c0ecf52a77f
Jeremy Pellow <jeremy.pellow@gmail.com>
2019-11-09 10:26:27 -0600

    Everything working, should clean up code and create more functions

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
     load();
     function load(){
         localStorage.setItem("e10", "do things");
+        var date = $("<div>").text(moment().format('dddd Do MMMM'));
+        date.addClass("display-5 time")
+        $(".display-5").append(date);
+        console.log(moment().format('h'));
+
         for (i=9; i < 18; i++){
         var newRow = $("<div>").addClass("row");
         var hour = $("<div>").addClass("col-1 text-right");
@@ -10,6 +15,7 @@ $(document).ready(function () {
         var save = $("<button>").addClass("btn btn-info col-1 text-center");
         hour.attr("id", "hour");
         task.attr({id: "task", type:"text", data: "e"+i});
+        task.addClass("live");
         task.val(localStorage.getItem("e"+i))
         save.attr({type: "button", id: "save", data: "e"+i});
         save.text("Save");
@@ -24,19 +30,28 @@ $(document).ready(function () {
         else{
             hour.text(i + "AM");
         }
+        if (i < moment().format('h')){
+            task.addClass("dead");
+        }
+        else if (i == moment().format('h')){
+            task.addClass("current");
+        }
         $("#hourRows").append(newRow);
         $(newRow).append(hour, task, save);
         // $(newRow).append(task);
         // $(newRow).append(save);
         };
-        i++
+        // i++
     }
     $(document).on("click", "#save", saveTask);
+    function bgColor(){
+        
+    }
+
     function saveTask(){
         var data = $(this).attr("data");
         var task = $("input[data="+data+"]").val();
-        console.log(data);
-        console.log(task);
         localStorage.setItem(data, task);
     }
+    
 });
